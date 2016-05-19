2a695300476c864f8dddb62f6f77e14a42bc428e
Alex Tomkins <tomkins@darkzone.net>
2016-05-19 14:14:41 +0100

    Should now support Python 3

--- blancseo/models.py
+++ blancseo/models.py
@@ -1,8 +1,12 @@
+from __future__ import unicode_literals
+
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 
 
+@python_2_unicode_compatible
 class MetaContent(models.Model):
     description = models.TextField(blank=True)
     keywords = models.TextField(blank=True)
@@ -15,10 +19,11 @@ class MetaContent(models.Model):
             ('content_type', 'object_id'),
         )
 
-    def __unicode__(self):
-        return u'Meta'
+    def __str__(self):
+        return 'Meta'
 
 
+@python_2_unicode_compatible
 class URLMetaContent(models.Model):
     url = models.CharField('URL', max_length=200, unique=True)
     description = models.TextField(blank=True)
@@ -27,5 +32,5 @@ class URLMetaContent(models.Model):
     class Meta:
         verbose_name = 'URL meta content'
 
-    def __unicode__(self):
+    def __str__(self):
         return self.url
