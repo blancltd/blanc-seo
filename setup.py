cfa083878f8117d2d1a25291d066d47fa23a7ffd
Alex Tomkins <tomkins@darkzone.net>
2016-05-19 13:21:21 +0100

    Improve setup.py

--- setup.py
+++ setup.py
@@ -1,33 +1,32 @@
 #!/usr/bin/env python
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+from codecs import open
+
+from setuptools import find_packages, setup
+
+
+with open('README.rst', 'r', 'utf-8') as f:
+    readme = f.read()
 
 
 setup(
     name='blanc-seo',
     version='0.1',
     description='Blanc SEO for Django',
-    long_description=open('README.rst').read(),
-    url='http://www.blanctools.com/',
-    maintainer='Alex Tomkins',
-    maintainer_email='alex@blanc.ltd.uk',
+    long_description=readme,
+    url='https://github.com/blancltd/blancseo',
+    maintainer='Blanc Ltd',
+    maintainer_email='studio@blanc.ltd.uk',
     platforms=['any'],
-    packages=[
-        'blancseo',
-        'blancseo',
-        'blancseo.templatetags',
-    ],
-    package_data={'blancseo': [
-        'seo/templates/seo/*.html',
-    ]},
+    packages=find_packages(),
+    include_package_data=True,
     classifiers=[
         'Environment :: Web Environment',
         'Framework :: Django',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
     ],
-    license='BSD-2',
+    license='BSD',
 )
